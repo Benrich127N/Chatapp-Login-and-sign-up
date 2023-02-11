3aa057f405c90c87f7a2d270ae1ae04527f6c008
Benrich127N <benrichprince1@gmail.com>
2023-02-11 04:15:38 +0100

    chatApp

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// ignore for file
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:login/auth/main_page.dart';
+import 'firebase_options.dart';
+import 'pages/login_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
